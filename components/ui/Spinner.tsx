17b81c33a17d895efe3a735429ea01ce3943de0e
r0bVious <robert.spano@gmail.com>
2024-09-04 21:03:59 +0900

    trying to fix the spinner issue

--- components/ui/Spinner.tsx
+++ components/ui/Spinner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Spinner = () => {
   return (
-    <div className="h-full w-full flex items-center justify-center">
+    <div className="h-[50%] w-full lg:h-full lg:w-[50%] flex items-center justify-center">
       <div className="relative size-28">
         <div className="absolute size-28 rounded-full border-[5px] border-customGranite border-t-transparent border-b-transparent animate-spin-slow"></div>
         <div className="absolute size-28 rounded-full border-[5px] border-customBlue border-t-transparent border-b-transparent animate-spin-slower"></div>
